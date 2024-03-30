25c8116904e3b8b5a3b129ba7de85fb3a5b4f4a6
admin <naresh.nagineni09@gmail.com>
2024-03-31 02:58:26 +0530

    initial commit

---
+++ mavendemo/src/main/java/com/learnmavendemo/App.java
@@ -0,0 +1,13 @@
+package com.learnmavendemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
